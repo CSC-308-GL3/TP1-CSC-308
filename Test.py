72f8753ca26d1f1379397f26a9478c351eb881f2
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-03-01 20:27:08 +0000

    Finalisation 2

--- Test.py
+++ Test.py
@@ -2,25 +2,22 @@ from Navire import Navire
 from Port import Port
 from Stockage import Stockage
 
+navire = Navire("0092493603", "ESMERALDA", "sac de riz", 1200)
 
-class Test:
-  def __init__(self):
-    navire = Navire("0092493603", "ESMERALDA", "sac de riz", 1200)
+silo1 = Stockage(100)
+silo2 = Stockage(200)
+silo3 = Stockage(300)
 
-    silo1 = Stockage(100)
-    silo2 = Stockage(200)
-    silo3 = Stockage(300)
+hangar1 = Stockage(1000)
+hangar2 = Stockage(1000)
 
-    hangar1 = Stockage(1000)
-    hangar2 = Stockage(1000)
+hangar1.stocker(500)
+hangar2.stocker(250)
 
-    hangar1.stocker(500)
-    hangar2.stocker(250)
+zonesDeStockage = [silo1, silo2, silo3, hangar1, hangar2]
 
-    zonesDeStockage = [silo1, silo2, silo3, hangar1, hangar2]
+port = Port(zonesDeStockage)
 
-    port = Port(zonesDeStockage)
+port.dechargement(navire)
 
-    port.dechargement(navire)
-
-    print("La quantité Fret restant : ",navire.obtenirQteFret())
+print("La quantité Fret restant : ",navire.obtenirQteFret())
