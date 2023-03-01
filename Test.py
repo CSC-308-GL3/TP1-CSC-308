9f2b1788037175ba57a4ac6b0e8bffaba6a86880
GenesisXV <64419828+GenesisXV@users.noreply.github.com>
2023-03-01 19:39:21 +0000

    test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-from Navire import Navire
-from Port import Port
-
-
-class Test:
-  def __init__(self):
-    self.__port = Port()
-    self.__navire = Navire("0092493603", "ESMERALDA", "sac de riz", 1200)
